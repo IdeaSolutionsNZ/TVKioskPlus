fd82ed1597db2a97dce46561e71757663509e3ba
IdeaSolutionsNZ <josh10@windowslive.com>
2026-02-19 21:43:54 +1300

    Delete src directory

--- src/screens/settings/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * FreeKiosk v1.2 - Settings Screen Export
- */
-
-export { default as SettingsScreenNew } from './SettingsScreenNew';
-export { default as SettingsScreen } from './SettingsScreenNew'; // Use new screen as default
--- src/screens/settings/tabs/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * FreeKiosk v1.2 - Settings Tabs Export
- */
-
-export { default as GeneralTab } from './GeneralTab';
-export { default as DisplayTab } from './DisplayTab';
-export { default as SecurityTab } from './SecurityTab';
-export { default as AdvancedTab } from './AdvancedTab';
